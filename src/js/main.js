235501f28659c878b4707ea597a2fa640116b24c
ilya-afonin <rjiabkom@gmail.com>
2018-09-11 18:25:44 +0300

    Add libs

--- src/js/main.js
+++ src/js/main.js
@@ -4,3 +4,82 @@
 // import 'jquery';
 require('jquery');
 
+$(document).ready(function () {
+
+    //mera();
+
+    headerScroll();
+    headerMobile();
+
+    /*mask();
+
+    callbackFormValidate();
+
+    popupFormValidate();
+
+    questionsFormValidate();
+
+    youtube();
+
+    gallery();
+
+    popup();
+
+    fancy();
+
+    nSlider();
+
+    showMoreText();
+
+    dsSlider();
+
+    wSlider();
+
+    scrollbar();
+
+    tabs();
+
+    faqItems();
+
+    wowJs();
+
+
+
+    fSlider();*/
+
+});
+
+
+
+function headerMobile() {
+
+    if ($('.header').length > 0 ) {
+
+        $('body').on('click', '.header__burger', function() {
+
+            $('.header').toggleClass('header--mob');
+
+            if ( (!($('.header').hasClass('header--custom'))) && (!($('.header').hasClass('header--fill'))) ) {
+
+                $('.header').toggleClass('header--custom');
+            }
+        });
+    }
+}
+
+function headerScroll() {
+
+    $(window).on('scroll', function () {
+
+        var scrolled = $(window).scrollTop();
+
+        if (scrolled > 100) {
+
+            $('.header').addClass('header--custom');
+
+        } else {
+
+            $('.header').removeClass('header--custom');
+        }
+    });
+}
